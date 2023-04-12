73d035ef26adf608c4b338a8ddd0b988a912b8f5
Julimar Jr <jjunior@xcapitalbank.com.br>
2023-04-12 19:18:40 -0300

    Desativação CronJobs

--- cronjobs.js
+++ cronjobs.js
@@ -3,9 +3,9 @@ const cron = require('node-cron')
 const AutoUpdateLinkAccount = require('./src/helpers/AutoUpdateLinkAccount')
 
 async function cronjobs(client) {
-    cron.schedule('*/2 * * * *', () => {
-        AutoUpdateLinkAccount(client);
-    })
+    // cron.schedule('*/2 * * * *', () => {
+    //     AutoUpdateLinkAccount(client);
+    // })
 }
 
 module.exports = cronjobs;
