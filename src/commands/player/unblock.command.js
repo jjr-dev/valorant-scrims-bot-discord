cf0bb51135b0cc83bbaa2304a0c0146861d6e2fa
Julimar Jr <jjunior@xcapitalbank.com.br>
2023-04-13 20:23:28 -0300

    Desbloqueio de jogador

---
+++ src/commands/player/unblock.command.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder  } = require('discord.js');
+
+const BlockedPlayerModel = require('../../models/blockedplayer.model');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("desbloquear")
+        .setDescription("Desbloquear jogador")
+        .addMentionableOption(option => 
+            option.setName('jogador')
+                .setDescription('Jogador que deseja desbloquear')
+        ),
+    async execute(interaction) {
+        await interaction.deferReply({
+            ephemeral: true
+        });
+
+        const member = interaction.options.getMentionable('jogador');
+
+        if(!member) {
+            await interaction.editReply(`Membro ${member} não encontrado`);
+            return;
+        }
+
+        const blocked = await BlockedPlayerModel.findOne({
+            user_id: interaction.user.id,
+            blocked_id: member.user.id
+        });
+
+        if(!blocked) {
+            await interaction.editReply(`Jogador ${member} não bloqueado`);
+            return;
+        }
+
+        await BlockedPlayerModel.deleteOne({
+            _id: blocked.id
+        });
+
+        await interaction.editReply(`O jogador ${member} foi desbloqueado`);
+    }
+}
